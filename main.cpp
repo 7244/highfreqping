a9e1b145430a85e29ce3f0bb990fbad60d63ef49
7244 <65ad4d@gmail.com>
2026-05-05 09:15:41 +0000

    recv from other thread now

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <WITCH/T/T.h>
 #include <WITCH/NET/NET.h>
 
+#include <thread>
 #include <print>
 
 static void client(const char *p){
@@ -11,16 +12,52 @@ static void client(const char *p){
     __abort();
   }
 
+  alignas(64) uint8_t ring[1024] = {};
+
+  uint64_t producer_index = 0;
+
   NET_socket_t sock;
-  if(NET_socket2(NET_AF_INET, NET_SOCK_DGRAM | NET_SOCK_NONBLOCK, NET_IPPROTO_UDP, &sock)){
+  if(NET_socket2(NET_AF_INET, NET_SOCK_DGRAM, NET_IPPROTO_UDP, &sock)){
     __abort();
   }
 
-  auto ns_per = (uint64_t)1'000'000;
-  auto inaccuracy_time_divide = (uint64_t)100;
+  std::thread t([&](){
+    while(1){
+      uint64_t ci;
+      NET_addr_t from;
+      auto recv_len = NET_recvfrom(&sock, &ci, sizeof(ci), &from);
+      if(recv_len < 0){
+        __abort();
+      }
+      else if(recv_len == sizeof(ci)){
+        if(from.ip == addr4port.ip && from.port == addr4port.port){
+          auto pi = __atomic_load_n(&producer_index, __ATOMIC_SEQ_CST);
+          if(ci + (sizeof(ring) / sizeof(ring[0])) <= pi){
+            std::print(
+              "fail: consumer is too late for producer.\n"
+              "producer: {} consumer: {}\n",
+              pi, ci
+            );
+            continue;
+          }
+  
+          auto *rv = &ring[ci % (sizeof(ring) / sizeof(ring[0]))];
+          if(!__atomic_load_n(rv, __ATOMIC_SEQ_CST)){
+            std::print(
+              "fail: unexpected reply.\n"
+              "producer: {} consumer: {}\n",
+              pi, ci
+            );
+            continue;
+          }
+          __atomic_store_n(rv, 0, __ATOMIC_SEQ_CST);
+        }
+      }
+    }
+  });
 
-  uint64_t i = 0;
-  uint8_t ring[1024 / 8] = {};
+  auto ns_per = (uint64_t)1'000'000;
+  auto inaccuracy_time_divide = (uint64_t)10;
 
   auto wanted_time = T_nowi();
 
@@ -29,26 +66,6 @@ static void client(const char *p){
     auto wanted_time_early = wanted_time - ns_per / inaccuracy_time_divide;
 
     while(1){
-      uint64_t recv_i;
-      NET_addr_t from;
-      auto recv_len = NET_recvfrom(&sock, &recv_i, sizeof(recv_i), &from);
-      if(recv_len == -EAGAIN){
-        /* ~~ */
-      }
-      else if(recv_len < 0){
-        __abort();
-      }
-      else if(recv_len == sizeof(recv_i)){
-        if(from.ip == addr4port.ip && from.port == addr4port.port){
-          uint8_t rvs = 1 << recv_i % 8;
-          auto *rv = &ring[recv_i / 8 % sizeof(ring)];
-          if(!(*rv & rvs)){
-            std::print("fail: unexpected reply {}\n", recv_i);
-          }
-          *rv &= ~rvs;
-        }
-      }
-
       if(T_nowi() >= wanted_time_early){
         break;
       }
@@ -56,24 +73,23 @@ static void client(const char *p){
       __processor_relax();
     }
 
+    auto *v = &ring[producer_index % (sizeof(ring) / sizeof(ring[0]))];
+    if(__atomic_load_n(v, __ATOMIC_SEQ_CST)){
+      std::print("{} didnt got reply\n", producer_index - (sizeof(ring) / sizeof(ring[0])));
+    }
+    __atomic_store_n(v, 1, __ATOMIC_SEQ_CST);
+
     while(1){
-      auto r = NET_sendto(&sock, &i, sizeof(i), &addr4port);
+      auto r = NET_sendto(&sock, &producer_index, sizeof(producer_index), &addr4port);
       if(r < 0){
         __abort();
       }
-      if(r == sizeof(i)){
+      if(r == sizeof(producer_index)){
         break;
       }
     }
 
-    uint8_t vs = 1 << i % 8;
-    auto *v = &ring[i / 8 % sizeof(ring)];
-    if(*v & vs){
-      std::print("{} didnt got reply\n", i - sizeof(ring) * 8);
-    }
-    *v |= vs;
-
-    i += 1;
+    __atomic_add_fetch(&producer_index, 1, __ATOMIC_SEQ_CST);
 
     auto now = T_nowi();
     auto diff = (sint64_t)now - (sint64_t)wanted_time;
